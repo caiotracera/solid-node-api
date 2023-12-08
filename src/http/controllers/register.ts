bb70662461156b53e80886aff881554d2cc1f286
Caio Tracera <ctrcra@gmail.com>
2023-12-08 19:05:03 +0000

    feat: add hash to password

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { FastifyRequest, FastifyReply } from 'fastify';
+import { hash } from 'bcryptjs';
 
 import { prisma } from '@/lib/prisma';
 
@@ -12,11 +13,13 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, password, email } = registerBodySchema.parse(request.body);
 
+  const password_hash = await hash(password, 6);
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     },
   });
 
